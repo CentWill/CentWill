043af86b8323551811a1498a7ba67f040e3ac426
lee <lee@leetekiMacBook-Pro.local>
2019-08-23 18:41:16 +0800

    resources are packaged into bundle hosted to Git; push related library upgrades, adapt to iOS 13; optimizing network shutdown and network switching logic. version changed to 1.2.0.8

--- Reohigh.framework/Headers/Reohigh.h
+++ Reohigh.framework/Headers/Reohigh.h
@@ -2,12 +2,11 @@
 //  Reohigh.h
 //  Reohigh
 //
-//  Created by MF on 2019/6/11.
-//  Copyright © 2019年 Reohigh. All rights reserved.
+//  Created by lee on 8/23/19.
+//  Copyright © 2019 leer. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import "ReoSignIn.h"
 //version:1.2.0.8, resources are packaged into bundle hosted to Git; push related library upgrades, adapt to iOS 13; optimizing network shutdown and network switching logic.
 
 //! Project version number for Reohigh.
